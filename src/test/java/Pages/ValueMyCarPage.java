89e2f51c363a9deb17d04f1458681f1da08b6797
Viswalatha.srivella <viswalatha.srivella@daemonsolutions.com>
2022-11-07 13:17:16 +0000

    modified

--- src/test/java/Pages/ValueMyCarPage.java
+++ src/test/java/Pages/ValueMyCarPage.java
@@ -1,11 +1,16 @@
 package Pages;
+import net.serenitybdd.core.pages.PageObject;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import util.CarDetails;
 import java.io.IOException;
+import java.time.Duration;
 
-public class ValueMyCarPage{
+
+public class ValueMyCarPage extends PageObject {
 
     CarDetails carDetails = new CarDetails();
     
@@ -21,9 +26,7 @@ public class ValueMyCarPage{
 
     By startValuation = By.xpath("//button[@type='submit']");
 
-    By getRegNumber = By.xpath("//*[@id='main-content']/div/div[2]/div/div/div/div[1]/div[1]/div/div/div[1]/div[2]/p");
-
-    By makeAndModel = By.xpath("//*[@id='main-content']/div/div[2]/div/div/div/div[1]/div[1]/div/div/div[1]/div[2]/p[2]");
+    By makeAndModel = By.xpath("//*[contains(text(),'Make/model')]");
 
     By errorMessage = By.xpath("//*[@id='your-registration-number-form']/div/div[1]/span");
 
@@ -31,6 +34,7 @@ public class ValueMyCarPage{
     public void getRegAndverifyCarDetails(WebDriver driver) throws IOException, InterruptedException {
 
         driver.findElement(acceptCookies).click();
+
         for (int i = 0; i < carDetails.extractRegNumbers().size(); i++) {
             if (i == 0) {
                 driver.findElement(enterReg).sendKeys(carDetails.extractRegNumbers().get(i));
@@ -41,31 +45,25 @@ public class ValueMyCarPage{
                 driver.findElement(enterReg).sendKeys(carDetails.extractRegNumbers().get(i));
                 driver.findElement(startValuation).click();
             }
-            Thread.sleep(3000);
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
             String carReg = carDetails.extractRegNumbers().get(i);
-            switch (carReg) {
-                case "AD58 VNF":
-                    Assert.assertTrue("Car reg number not found or not equal",driver.findElement(getRegNumber).getText().contains(carDetails.expetedCarDetails().get(2).getRegistration()));
-                    Assert.assertTrue("Car make not found or not equal", driver.findElement(makeAndModel).getText().contains(carDetails.expetedCarDetails().get(2).getMakeAndModel()));
-                    break;
-                case "BW57 BOW":
-                    Assert.assertTrue("Car reg number not found ",driver.findElement(errorMessage).isDisplayed());
-                    break;
-                case "KT17DLX":
-                    Assert.assertTrue("Car reg number not found or not equal",driver.findElement(getRegNumber).getText().contains(carDetails.expetedCarDetails().get(4).getRegistration()));
-                    Assert.assertTrue("Car make not found or not equal", driver.findElement(makeAndModel).getText().contains(carDetails.expetedCarDetails().get(4).getMakeAndModel()));
-                    break;
-                case "SG18 HTN":
-                    Assert.assertTrue("Car reg number not found or not equal",driver.findElement(getRegNumber).getText().contains(carDetails.expetedCarDetails().get(1).getRegistration()));
-                    Assert.assertTrue("Car make not found or not equal", driver.findElement(makeAndModel).getText().contains( carDetails.expetedCarDetails().get(1).getMakeAndModel()));
-                    break;
-                default:
-                    System.out.println("Reg number " + carReg + "not found");
+            for(int k = 0; k < carDetails.expetedCarDetails().size(); k++){
+                if(carDetails.expetedCarDetails().contains(carReg))
+                    if(wait.until(ExpectedConditions.visibilityOfElementLocated(makeAndModel)).isDisplayed() ) {
+                        Assert.assertTrue("Car make not found or not equal", driver.findElement(makeAndModel).getText().contains(carDetails.expetedCarDetails().get(k).getMakeAndModel()));
+                    }
+                    else{
+                        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage)).isDisplayed());
+                    }
+                }
+
             }
+
+
         }
 
     }
 
 
-
-}
--- src/test/java/steps/CarDetailsStepDefinitions.java
+++ src/test/java/steps/CarDetailsStepDefinitions.java
@@ -7,8 +7,6 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
 import java.io.IOException;
 
 
